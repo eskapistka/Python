5f7374402b6fd1a8f7c384c7016209af17e0142b
Wiktoria <49492974+eskapistka@users.noreply.github.com>
2020-11-18 14:37:01 +0000

    Update sublists_of_the_same_characters_from_a_list.py

--- sublists_of_the_same_characters_from_a_list.py
+++ sublists_of_the_same_characters_from_a_list.py
@@ -1,25 +1,34 @@
-# This code returns a list of lists. Every list inside the output list contains the same character (all of its occurences in the main input list)
+# This code prints a list of lists, every list in the output list contains
+# every occurrence of the same character, note that '1' is different than 1
 
-# Expect one input from user
-usr_input = input("Enter a list: ")
-user_list = usr_input[1:-1].strip().replace(' ', '').replace('"', "'").split(',')
-dictionary = {}
-output_list = []
+def list_of_lists(input_list):
+    input_list = str(input_list)
+    user_list = input_list[1:-1].strip().replace(' ', '').replace('"', "'").split(',')
+    dictionary = {}
+    output_list = []
 
-for element in user_list:
-    if element[:1] and element[-1:] == "'":
-        element = element[1:-1]
-    else:
-        if 96 < ord(element) < 123:
-            pass
+    for element in user_list:
+        if element[:1] and element[-1:] == "'":
+            element = element[1:-1]
         else:
-            element = int(element)
-    if element in dictionary:
-        dictionary[element] += 1
-    else:
-        dictionary[element] = 1
+            if 96 < ord(element) < 123:
+                pass
+            else:
+                element = int(element)
+        if element in dictionary:
+            dictionary[element] += 1
+        else:
+            dictionary[element] = 1
 
-for key in dictionary:
-    output_list.append([key] * dictionary[key])
+    for key in dictionary:
+        output_list.append([key] * dictionary[key])
 
-answer = output_list
+    return output_list
+
+# Simple test for the function (using pytest)
+
+def test_answer():
+    assert list_of_lists([2, 1, 2, 1]) == [[2, 2], [1, 1]]
+    assert list_of_lists([5, 4, 5, 5, 4, 3]) == [[5, 5, 5], [4, 4], [3]]
+    assert list_of_lists([2,1,2,1]) == [[2, 2], [1, 1]]
+    assert list_of_lists([1,2,1,3,'a','b',"a", 'c', 'a',"a", "a", '1',"1"]) == [[1, 1], [2], [3], ['a', 'a', 'a', 'a', 'a'], ['b'], ['c'], ['1', '1']]
