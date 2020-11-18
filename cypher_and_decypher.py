60efe9bf4a2253213b16f5c77656d468fddf586c
Wiktoria <49492974+eskapistka@users.noreply.github.com>
2020-11-18 01:50:30 +0000

    Update and rename cypher-and-decypher.py to cypher_and_decypher.py

--- cypher_and_decypher.py
+++ cypher_and_decypher.py
@@ -1,25 +1,13 @@
-# cw2.py (template file)
-
 import csv
 
-
-# =============================================================================
-# (a) Key checker function
 # This function checks if the key is between 0 and 94.
-# =============================================================================
 def check_key(key):
     # Replace the following lines with your code
     if key in range(0, 95):
         return True
     return False
 
-
-# =============================================================================
-# (b) Function to count number of upper case and lower case letters
-# This function counts the number of upper case and lower case letter of a
-# given string.
-# =============================================================================
-
+# This function counts the number of upper case and lower case letter of a given string.
 def check_upper_lower(s):
     upper = 0
     lower = 0
@@ -32,16 +20,13 @@ def check_upper_lower(s):
 
     return [upper, lower]
 
-
-# =============================================================================
-# (c) Function to encrypt or decrypt a string
 # Encrypt or decrypt a string s1 with a key based on the option
 # and return a string value.
 # Option = 1 for encrypt, option =2 for decrypt, default option =1,
 # other option value returns message "Invalid option!".
 # Key value between 0 and 94, default key=3. Other key value returns
 # "Invalid key!"
-# =============================================================================
+
 def encrypt_decrypt_string(s1, key=3, option=1):
     # For invalid option
     if option != 1 and option != 2:
@@ -69,19 +54,9 @@ def encrypt_decrypt_string(s1, key=3, option=1):
 
     return output
 
-
-# This is an example of how to call this function without providing key argument
-# print(encrypt_decrypt_string('Khoor#Zruog#=,', option = 2))
-# Calling this function without the option argument
-# print(encrypt_decrypt_string('Khoor#Zruog#=,', 3))
-
-# =============================================================================
-# (d) Function to encrypt or decrypt content of a text file
-# Encrypt and decrypt a txt file (infile) based on the key and option, call to
-# function in part (c) to encrypt each string in the file. Output the encrypted
+# Encrypt and decrypt a txt file (infile) based on the key and option. Output the encrypted
 # string to a different txt file (outfile) and print "Wrong file or file path"
-# if infile is not found. Do not forget the default key and option as in (c).
-# =============================================================================
+# if infile is not found. 
 
 def encrypt_decrypt_text_in_file(infile, outfile, key=3, option=1):
     try:
@@ -100,16 +75,9 @@ def encrypt_decrypt_text_in_file(infile, outfile, key=3, option=1):
     except (FileNotFoundError, IOError):
         print("Wrong file or file path")
 
-
-encrypt_decrypt_text_in_file('input.txt', 'output.txt', 3, 1)
-
-
-# =============================================================================
-# (e) Function to count occurrence of words in a text file
 # This function count the occurrences of each word in a txt file (sfile) and
 # returns the occurences of each word in a dictionary format. It returns
 # "Wrong file or file path" if sfile is not found.
-# =============================================================================
 def count_words_in_file(sfile):
     # Key to the dictionary is the word in uppercase and the value is the number of its occurences
     wordlist = {}
@@ -137,14 +105,8 @@ def count_words_in_file(sfile):
     except (FileNotFoundError, IOError):
         return "Wrong file or file path"
 
-
-# =============================================================================
-# (f) Function to find max, min, sum, count, and mean
-# (Not required for COMP0011 (IFY) students)
 # This function find the maximum, minimum, sum, count, and mean values of
-# a column (as specified by col_index) in a txt file (infile). Note that
-# col_index starts with a 0 (zero).
-# =============================================================================
+# a column (as specified by col_index) in a txt file (infile). 
 def summarise_data(infile, col_index):
     try:
         with open(infile) as input_file:
